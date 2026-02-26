fc1a878c5d0d2e9decc3a445241b03bb295c21e8
Felipe Delestro <fima@dtu.dk>
2026-02-26 12:46:58 +0100

    Rotating plot and more

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
 import './shared/toggler'
+
+const fadeInElementsOnPageLoad = () => {
+	const elements = document.querySelectorAll('[data-fade-on-load]')
+
+	elements.forEach((element) => {
+		requestAnimationFrame(() => {
+			element.classList.remove('opacity-0')
+		})
+	})
+}
+
+if (document.readyState === 'loading') {
+	document.addEventListener('DOMContentLoaded', fadeInElementsOnPageLoad)
+} else {
+	fadeInElementsOnPageLoad()
+}
